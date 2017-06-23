bf395c0380ddf018851627b4de51415be4b687fb
wutong <tong.wu@netviewtech.com>
2017-06-23 20:24:29 +0800

    正则表达式加字符串

---
+++ oc/NSStringN.m
@@ -0,0 +1,3 @@
+/private/var/mobile/Containers/Data/Application/B830-DEAEB7071D98/tmp//deviceVideoEventNVT/23232/Video/sdsdsd/sdsdsd.zip
+/private/var/mobile/Containers/Data/Application/B830-DEAEB7071D98/tmp//deviceVideoEventNVT/23232/Video/sdsdsd
+[replacedStr stringByDeletingLastPathComponent]
