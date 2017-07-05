164ea970570e89657957df83ece3cb72ff8dd05c
wutong <tong.wu@netviewtech.com>
2017-07-05 08:56:33 +0800

    1. 字符串截取相关

--- oc/NSStringN.m
+++ oc/NSStringN.m
@@ -1,3 +1,16 @@
 /private/var/mobile/Containers/Data/Application/B830-DEAEB7071D98/tmp//deviceVideoEventNVT/23232/Video/sdsdsd/sdsdsd.zip
 /private/var/mobile/Containers/Data/Application/B830-DEAEB7071D98/tmp//deviceVideoEventNVT/23232/Video/sdsdsd
 [replacedStr stringByDeletingLastPathComponent]
+
+
+// 拿testName
+NSString *str2 = @"Users/CentralPerk/Desktop/testName.txt";
+str2 = [str2 stringByDeletingPathExtension];
+str2 = [str2 lastPathComponent];
+NSLog(@"%@",str2);
+
+// NSString *str2 = @"Users/CentralPerk/Desktop/testName---->
+// NSString *str2 = @"Users/CentralPerk/Desktop/testName.NVT3
+NSString *str2 = @"Users/CentralPerk/Desktop/testName";
+str2 = [str2 stringByAppendingPathExtension:@"NVT3"]
+    NSLog(@"%@", str2);
