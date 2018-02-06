dbfa124869f47c2190421b54a2f1e5950da8d164
wutong <tong.wu@netviewtech.com>
2018-02-06 18:18:24 +0800

    TreeSet

--- java/code/newStudy/src/main/java/HashSetDemo/TreeSetDemo.java
+++ java/code/newStudy/src/main/java/HashSetDemo/TreeSetDemo.java
@@ -1,13 +1,64 @@
 package HashSetDemo;
 
+import lombok.Setter;
+
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
  * Created by codew on 2018/2/1.
  */
+
+@Setter
+class TreeSetPerson implements Comparable
+{
+
+    private int age;
+    private String name;
+
+    public TreeSetPerson(String name, int age){
+
+        this.age = age;
+        this.name = name;
+
+    }
+
+    public String toString(){
+
+        return "TreeSetPerson [name" + name + ", age=" + age + "]";
+    }
+
+    @Override
+    public int compareTo(Object o) {
+
+        TreeSetPerson person = (TreeSetPerson)o;
+        int personAge = person.age;
+        int selfAge = this.age;
+
+        return  selfAge - personAge;
+    }
+}
+
+
 public class TreeSetDemo {
 
     public static void main(String[] args){
+
+
+        Set<TreeSetPerson> set = new TreeSet<TreeSetPerson>();
+
+        set.add(new TreeSetPerson("周星驰", 28));
+        set.add(new TreeSetPerson("李小龙", 29));
+        set.add(new TreeSetPerson("威尔史密斯", 24));
+        set.add(new TreeSetPerson("杰森·斯坦森", 26));
+
+        System.out.println(set);
+    }
+
+
+
+
+    public static void test(){
 
         TreeSet<String> set = new TreeSet<>();
 
