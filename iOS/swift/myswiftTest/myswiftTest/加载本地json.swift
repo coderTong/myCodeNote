aa29a10476dd2e87e99d921e3c3b35f7dd7122b5
wutong <codewutong@foxmail.com>
2018-06-15 20:39:57 +0800

    Swift update

---
+++ iOS/swift/myswiftTest/myswiftTest/加载本地json.swift
@@ -0,0 +1,22 @@
+//
+//  加载本地json.swift
+//  myswiftTest
+//
+//  Created by codew on 2018/6/12.
+//  Copyright © 2018年 codew. All rights reserved.
+//
+
+import Foundation
+
+
+//// 1.获取json文件路径
+//guard let jsonPath = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil) else {
+//    print("没有获取到对应的文件路径")
+//    return
+//}
+//
+//// 2.读取json文件中的内容
+//guard let jsonData = NSData(contentsOfFile: jsonPath) else {
+//    print("没有获取到json文件中数据")
+//    return
+//}
